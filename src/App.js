28b9cba7a0d2dd900c409768746db212f170ccae
erkutsakar <erkut.sakar@hotmail.com>
2023-12-18 00:09:03 +0300

    add react bootstrap to project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Button from 'react-bootstrap/Button';
+
 
 function App() {
+
+const [counter, setCounter] = useState(0);
+
+useEffect( () => {
+  console.log(counter)
+}, [counter] )
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>{counter}</div>
+     <button onClick={ () => setCounter(prevCounter => prevCounter  + 1 ) }>increment</button>
+<Button>Hello</Button>
     </div>
   );
 }
