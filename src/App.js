2adabfa842c45f2ed7c3ac74d06a0cb9fe11c73e
Erkut Şakar <esakar@fnss.com.tr>
2023-12-18 16:24:20 +0300

    useState ve formcontrol eklenmesi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 
 import {Form, Button} from 'react-bootstrap';
+import React, { useEffect } from 'react'; 
+import { useState } from 'react';
 
 function App() {
 
 
-
-
+const [todo, setTodo] = useState('')
+useEffect(
+  () =>{console.log(todo)},[todo]
+)
 
   return (
     <div className="d-flex flex-column justify-content-center align-items-center mt-5">
  
       <h1 className="mt-5">Todo List</h1>
-      <div className='d-flex w-60'>  
+      <div className='d-flex w-50'>  
         <Form.Control
-          placeholder="Username"
-          aria-label="Username"
-          aria-describedby="basic-addon1"
+          placeholder="Todo Input"  
+          value= {todo}  
+          onChange={(e) => setTodo(e.target.value) }      
         />
-         <Button>Add Todo</Button>
+         <Button className='ms-5' onClick={() => console.log({todo})}>Add Todo</Button>
          </div>
     </div>
   );
