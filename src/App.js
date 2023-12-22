7d5e9c61b57f96d088037454e9f6846f084a5a68
erkutsakar <erkutsakar@hotmail.com>
2023-12-23 00:15:14 +0300

    add delete update buttons added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 
 import {Form, Button} from 'react-bootstrap';
-
 import {  useState } from 'react';
+import  {ReactComponent as DeleteIcon} from './assets/deleteicon.svg' 
+import  {ReactComponent as SaveIcon} from './assets/saveicon.svg' 
+import  {ReactComponent as EditIcon} from './assets/editicon.svg' 
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
 
 const [todoList, setTodoList] = useState([])
 const [todo, setTodo] = useState('')
+const [newTodo, setNewTodo] = useState('')
 
 const addTodo = () =>
 {
-  setTodoList(prevTodoList => [...prevTodoList, todo])
-  setTodo('')
+  setTodoList(prevTodoList => [...prevTodoList, {id:uuidv4(), todo:newTodo, isEditable: false, isCompleted: false}])
+  setNewTodo('')
 }
 
+const completeTodo = (id) =>
+{
+  setTodoList(prevTodoList => prevTodoList.map(todoitem => todoitem.id === id ? {...todoitem, isCompleted: !todoitem.isCompleted} : todoitem ))
+}
+
+const editTodo = (id, oldTodo) =>
+{
+  setTodoList(prevTodoList => prevTodoList.map(todoitem => todoitem.id === id ? {...todoitem, isEditable: !todoitem.isEditable} : todoitem))  
+  setTodo(oldTodo);
+}
+
+const saveTodo = (id) =>
+{
+  setTodoList(prevTodoList => prevTodoList.map(todoitem => todoitem.id === id ? {...todoitem, isEditable: !todoitem.isEditable, todo:todo} : todoitem ))
+
+}
+const deleteTodo = (id) =>
+{
+  setTodoList(prevTodoList => prevTodoList.filter(todoitem => todoitem.id !== id ))
+
+}
 
 // test 2
 
@@ -24,8 +49,8 @@ const addTodo = () =>
       <div className='d-flex w-50'>  
         <Form.Control
           placeholder="Todo Input"  
-          value= {todo}  
-          onChange={(e) => setTodo(e.target.value) }      
+          value= {newTodo}  
+          onChange={(e) => setNewTodo(e.target.value) }      
         />
          <Button className='ms-5' onClick={() => {addTodo()} }>Add Todo</Button>
          </div>
@@ -33,14 +58,51 @@ const addTodo = () =>
          <div className='mt-5'>
           {
             todoList.map(
-              todoitem => <div key={todoitem}>{todoitem}</div>
-            )           }
+              (todoitem) => 
+                  <div key={todoitem.id} className="d-flex justify-content-between">
+                    <div className='d-flex w-75'>
+                     <Form.Check
+                        type="checkbox"
+                        className="me-2" 
+                        value= {todoitem.isCompleted}
+                        onChange={() =>completeTodo(todoitem.id)}
+                        />
+
+                        {
+                         !todoitem.isEditable ?   
+                        <label className={`${todoitem.isCompleted ? 'text-decoration-line-through': ''} fw-bold` }>
+                        {todoitem.todo}
+                        </label>
+                         :
+                         <Form.Control
+                          value= {todo}  
+                          onChange={(e) => setTodo(e.target.value) }      
+                        />
+
+                        }
+
+                        {
+                           !todoitem.isEditable ? 
+                        <div><EditIcon width={25} height={25} style={({cursor:'pointer'})} className='me-2 ms-5' onClick={() => editTodo(todoitem.id, todoitem.todo) }/></div>
+                         :
+                        <div><SaveIcon width={25} height={25} style={({cursor:'pointer'})} className='me-2' onClick={() => saveTodo(todoitem.id) }  /></div>
+                        }
+                        <div><DeleteIcon width={25} height={25} style={({cursor:'pointer'})} className='me-2' onClick={() => deleteTodo(todoitem.id) }  /></div>
+
+                        
+                        
+                    </div>
+
+                    </div>
+                    
+            )           
+          }
          </div>
     </div>
   
 
   
-  );
+  )
   }
 
 export default App;
